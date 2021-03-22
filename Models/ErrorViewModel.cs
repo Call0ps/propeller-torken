eb210cb64f11a215f09ac28dd088d26547eb50eb
Carl Emil Lidbom <lidbom.calle@gmail.com>
2021-03-22 11:34:54 +0100

    Created project template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace propeller_torken.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
