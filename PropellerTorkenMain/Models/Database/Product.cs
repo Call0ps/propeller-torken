ab4e8dbc13735805b6acbf8881917950912248e6
CarlMM <kaptenkeps@gmail.com>
2021-03-31 14:57:29 +0200

    Databases

--- PropellerTorkenMain/Models/Database/Product.cs
+++ PropellerTorkenMain/Models/Database/Product.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace PropellerTorkenMain.Models
+#nullable disable
+
+namespace PropellerTorkenMain.Models.Database
 {
-    public class Products
+    public partial class Product
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
         public int Qty { get; set; }
-
-
     }
 }
