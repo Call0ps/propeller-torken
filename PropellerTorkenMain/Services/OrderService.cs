b57b392a71a8abd6d1f58bad373e7f64b4ba2310
CarlMM <kaptenkeps@gmail.com>
2021-03-31 15:25:40 +0200

    Småpill

--- PropellerTorkenMain/Services/OrderService.cs
+++ PropellerTorkenMain/Services/OrderService.cs
@@ -11,23 +11,23 @@ namespace PropellerTorkenMain.Services
     {
 
         PropellerDataContext pdc = new PropellerDataContext();
-        public List<Orders> CurrentOrderList { get; set; }
-        public List<Orders> SentOrderList { get; set; }
+        public List<Order> CurrentOrderList { get; set; }
+        public List<Order> SentOrderList { get; set; }
 
         public OrderService()
         {
-            Products myProducts = new Products() { ID = 1, Name = "PropellerKeps1", Price = 150 };
-            Customer myCustomer = new Customer() { FirstName = "Carl", LastName = "Bajs", Address = "Bajsgatan 1", City = "Malmö", ZipCode = 12345, Email = "InteBajs@gmail.com", PhoneNr = "070812345678" };
-            Products myProducts2 = new Products() { ID = 2, Name = "Torktumlare", Price = 1500 };
-            Customer myCustomer2 = new Customer() { FirstName = "Anton", LastName = "Fitness", Address = "LundVägen 37", City = "Lund", ZipCode = 11223, Email = "AntonFitness@gmail.com", PhoneNr = "07089996633" };
+            Product myProducts = new Product() { Id = 1, Name = "PropellerKeps1", Price = 150 };
+            Customer myCustomer = new Customer() { Firstname = "Carl", Lastname = "Bajs", Address = "Bajsgatan 1", City = "Malmö", Zipcode = 12345, Email = "InteBajs@gmail.com", PhoneNr = "070812345678" };
+            Product myProducts2 = new Product() { Id = 2, Name = "Torktumlare", Price = 1500 };
+            Customer myCustomer2 = new Customer() { Firstname = "Anton", Lastname = "Fitness", Address = "LundVägen 37", City = "Lund", Zipcode = 11223, Email = "AntonFitness@gmail.com", PhoneNr = "07089996633" };
 
-            CurrentOrderList = new List<Orders>()
+            CurrentOrderList = new List<Order>()
             {
-                new Orders{Id = 1, OurCustomer = myCustomer, Date = DateTime.Now, OurProduct = new List<Products>{myProducts } },
-                new Orders{Id = 2, OurCustomer = myCustomer2, Date = DateTime.Now, OurProduct = new List<Products>{myProducts2}}
+                new Order{Id = 1, OurCustomer = myCustomer, Date = DateTime.Now, OurProduct = new List<Product>{myProducts } },
+                new Order{Id = 2, OurCustomer = myCustomer2, Date = DateTime.Now, OurProduct = new List<Product>{myProducts2}}
             };
 
-            SentOrderList = new List<Orders>();
+            SentOrderList = new List<Order>();
 
         }
 
