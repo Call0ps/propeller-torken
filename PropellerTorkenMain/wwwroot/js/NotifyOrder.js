4bf64b50f64e4df5e0b2cf159a8af9b597175a23
ELIN-ORTEGAS-LE\elinw <elin.w.ortega@gmail.com>
2021-04-12 16:26:28 +0200

    Fixat SignalR

--- PropellerTorkenMain/wwwroot/js/NotifyOrder.js
+++ PropellerTorkenMain/wwwroot/js/NotifyOrder.js
@@ -1,16 +1,16 @@
-﻿"use strict";
+﻿////"use strict";
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/orderHub").build();
+////var connection = new signalR.HubConnectionBuilder().withUrl("/orderHub").build();
 
-connection.start().then(function () {
-}).catch(function (err) {
-    return console.error(err.toString());
-});
+////connection.start().then(function () {
+////}).catch(function (err) {
+////    return console.error(err.toString());
+////});
 
-document.getElementById("CustomerConfirmButton").addEventListener("click", function (event) {
-    var message = "Det finns en ny order att hantera";
-    connection.invoke("SendMessage", message).catch(function (err) {
-        return console.error(err.toString());
-    });
-    event.preventDefault();
-});
+////document.getElementById("CustomerConfirmButton").addEventListener("click", function (event) {
+////    var message = "Det finns en ny order att hantera";
+////    connection.invoke("SendMessage", message).catch(function (err) {
+////        return console.error(err.toString());
+////    });
+////    /*event.preventDefault();*/
+////});
